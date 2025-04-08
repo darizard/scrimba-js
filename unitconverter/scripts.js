860ab8d00880fc3006538df77ce9b54bc9580dbf
darizard <jstrode84@gmail.com>
2025-04-08 18:09:59 +0000

    Add support for changing unit systems

--- unitconverter/scripts.js
+++ unitconverter/scripts.js
@@ -1,5 +1,5 @@
-import ConverterContainer from './classes/convertercontainer.js';
-import ConversionData from './classes/conversiondata.js';
+import ConverterContainer from './components/convertercontainer.js';
+import ConversionData from './data/conversiondata.js';
 
 const conversionData = new ConversionData();
 const converterContainers = [];
@@ -8,8 +8,6 @@ const typesEl = document.querySelector("#unit-type-select");
 const addBtn = document.querySelector("#add-converter-btn");
 const clearBtn = document.querySelector("#clear-converters-btn");
 const convAreaEl = document.querySelector("#converters-area");
-
-// let lastUnit;
 
 addBtn.addEventListener("click", createConverter);
 clearBtn.addEventListener("dblclick", clearConverters);
@@ -29,32 +27,16 @@ function createConverter() {
     const index = getAvailableConvIndex();
     const container = new ConverterContainer(typesEl.value, index);
     container.delBtn.addEventListener("click", deleteConverter);
-    container.measure1InputEl.addEventListener("input", curry_convertMeasureChanged(
-        container.unit1SelectEl,
-        container.unit2SelectEl,
-        container.measure2InputEl
-    ));
-    // container.measure2InputEl.addEventListener("input", curry_convertMeasureChanged(
-    //     container.unit2SelectEl,
-    //     container.unit1SelectEl,
-    //     container.measure1InputEl
-    // ));
-    // container.unit1SelectEl.addEventListener("focus", function(evt) {
-    //     lastUnit = evt.target.value;
-    // });
-    // container.unit2SelectEl.addEventListener("focus", function(evt) {
-    //     lastUnit = evt.target.value;
-    // });
-    container.unit1SelectEl.addEventListener("change", curry_convertUnitChangedLeft(
-        container.measure1InputEl,
-        container.unit2SelectEl,
-        container.measure2InputEl
-    ));
-    container.unit2SelectEl.addEventListener("change", curry_convertUnitChangedRight(
-        container.measure1InputEl,
-        container.unit1SelectEl,
-        container.measure2InputEl
-    ));
+    // Measurement to be converted is updated
+    container.measure1InputEl.addEventListener("input", curry_convertMeasureChanged(container));
+    // Source measurement unit is updated
+    container.unit1SelectEl.addEventListener("change", curry_convertUnitChangedLeft(container));
+    // Destination measurement unit is updated
+    container.unit2SelectEl.addEventListener("change", curry_convertUnitChangedRight(container));
+    // Source unit system is toggled
+    container.system1BtnEl.addEventListener("click", curry_systemChanged(container, true));
+    // Destination unit system is toggled
+    container.system2BtnEl.addEventListener("click", curry_systemChanged(container, false));
     // push a new value onto the containers array if necessary
     if(index === converterContainers.length) converterContainers.push(container);
     // otherwise use an empty slot
@@ -95,26 +77,79 @@ function clearConverters() {
     while(convAreaEl.children.length > 0) convAreaEl.removeChild(convAreaEl.lastChild);
 }
 
-function curry_convertMeasureChanged(fromUnitEl, toUnitEl, toMeasureEl) {
+// Value of the measurement is updated
+function curry_convertMeasureChanged(container) {    
     return function convertMeasureChanged(evt) {
-        let result = conversionData.convert(fromUnitEl.value, toUnitEl.value, evt.target.value);
+        const toMeasureEl = container.measure2InputEl;
+        // Convert to the destination unit
+        let result = conversionData.convert(
+            container.unit1SelectEl.value,
+            container.unit2SelectEl.value,
+            evt.target.value
+        );
         if(result >= 1000000 || result < 0.1) toMeasureEl.value = result.toPrecision(4);
         else toMeasureEl.value = result.toFixed(3);
     }
 }
 
-function curry_convertUnitChangedLeft(fromMeasureEl, toUnitEl, toMeasureEl) {
-    return function convertUnitChanged(evt) {
-        let result = conversionData.convert(evt.target.value, toUnitEl.value, fromMeasureEl.value);
+// Source unit is changed
+function curry_convertUnitChangedLeft(container) {
+    return function convertUnitChangedLeft(evt) {
+        const toMeasureEl = container.measure2InputEl
+        // Convert destination measurement based on new unit
+        let result = conversionData.convert(
+            evt.target.value, 
+            container.unit2SelectEl.value, 
+            container.measure1InputEl.value
+        );
         if(result >= 1000000 || result < 0.1) toMeasureEl.value = result.toPrecision(4);
         else toMeasureEl.value = result.toFixed(3);
     }
 }
 
-function curry_convertUnitChangedRight(fromMeasureEl, fromUnitEl, toMeasureEl) {
-    return function convertUnitChanged(evt) {
-        let result = conversionData.convert(fromUnitEl.value, evt.target.value, fromMeasureEl.value);
+// Destination unit is changed
+function curry_convertUnitChangedRight(container) {
+    return function convertUnitChangedRight(evt) {
+        const toMeasureEl = container.measure2InputEl;
+        // Convert destination measurement based on new unit
+        let result = conversionData.convert(
+            container.unit1SelectEl.value, 
+            evt.target.value, 
+            container.measure1InputEl.value
+        );
         if(result >= 1000000 || result < 0.1) toMeasureEl.value = result.toPrecision(4);
         else toMeasureEl.value = result.toFixed(3);
     }
 }
+
+// Either unit system toggled (imp/met)
+function curry_systemChanged(container, isLeft) {
+    return function systemChangedLeft(evt) {
+        // Update system and units
+        const selectToUpdate = isLeft ? container.unit1SelectEl : container.unit2SelectEl;
+        const fromSystem = evt.target.textContent;
+        const toSystem = fromSystem === "Metric" ? "Imperial" : "Metric";
+        evt.target.textContent = toSystem;
+        container.repopUnits(toSystem, container.unitTypeEl.textContent, isLeft);
+        const baseUnit = conversionData.getBaseUnit(toSystem,container.unitTypeEl.textContent);
+        selectToUpdate.value = baseUnit;
+
+        // Convert destination measurement based on new unit
+        let result = conversionData.convert(
+            container.unit1SelectEl.value,
+            container.unit2SelectEl.value,
+            container.measure1InputEl.value
+        );
+        if(result >= 1000000 || result < 0.1) container.measure2InputEl.value = result.toPrecision(4);
+        else container.measure2InputEl.value = result.toFixed(3);
+    }
+}
+
+// Destination unit system toggled (imp/met)
+function curry_systemChangedRight() {
+    return function systemChangedRight(evt) {
+        // Update system and units of right side
+
+    }
+}
+
